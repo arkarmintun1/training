8d215e137780c6fbf94e4e165feb4a7f1434ba2a
Arkar Min Tun <arkarmintun1@gmail.com>
2020-12-14 23:24:49 +0630

    react intro in plain html and javascript

---
+++ scoreboard/app.js
@@ -0,0 +1,18 @@
+const title = React.createElement(
+  'h1',
+  {
+    id: 'main-title',
+    title: 'This is a title',
+  },
+  'My First React Element!'
+);
+
+const description = React.createElement(
+  'p',
+  null,
+  'I just learned how to create a react node and render it into the DOM'
+);
+
+const header = React.createElement('header', null, title, description);
+
+ReactDOM.render(header, document.getElementById('root'));
