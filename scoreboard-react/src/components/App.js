e340e72c0083612d1581b56f7c3a9619a897e2b7
Arkar Min Tun <arkarmintun1@gmail.com>
2020-12-15 13:46:15 +0630

    added form for new user adding

--- scoreboard-react/src/components/App.js
+++ scoreboard-react/src/components/App.js
@@ -2,32 +2,35 @@ import React, { Component } from 'react';
 
 import Header from './Header';
 import Player from './Player';
+import AddPlayerForm from './AddPlayerForm';
 
 class App extends Component {
   state = {
     players: [
       {
         name: 'Guil',
-        score: 50,
+        score: 0,
         id: 1,
       },
       {
         name: 'Treasure',
-        score: 85,
+        score: 0,
         id: 2,
       },
       {
         name: 'Ashley',
-        score: 95,
+        score: 0,
         id: 3,
       },
       {
         name: 'James',
-        score: 80,
+        score: 0,
         id: 4,
       },
     ],
   };
+
+  prevPlayerId = 4;
 
   handleRemovePlayer = (id) => {
     this.setState((prevState) => ({
@@ -35,20 +38,43 @@ class App extends Component {
     }));
   };
 
+  handleScoreChange = (index, delta) => {
+    this.setState((prevState) => ({
+      score: (prevState.players[index].score += delta),
+    }));
+  };
+
+  handleAddPlayer = (name) => {
+    this.setState((prevState) => ({
+      players: [
+        ...prevState.players,
+        {
+          name,
+          score: 0,
+          id: (this.prevPlayerId += 1),
+        },
+      ],
+    }));
+  };
+
   render() {
     return (
       <div className="scoreboard">
-        <Header title="Scoreboard" totalPlayers={this.state.players.length} />
+        <Header title="Scoreboard" players={this.state.players} />
 
         {/* Player List */}
-        {this.state.players.map((player) => (
+        {this.state.players.map((player, index) => (
           <Player
             key={player.id.toString()}
             id={player.id}
             name={player.name}
+            score={player.score}
+            index={index}
             removePlayer={this.handleRemovePlayer}
+            changeScore={this.handleScoreChange}
           />
         ))}
+        <AddPlayerForm addPlayer={this.handleAddPlayer} />
       </div>
     );
   }
--- scoreboard-react/src/components/Player.js
+++ scoreboard-react/src/components/Player.js
@@ -14,7 +14,11 @@ const Player = (props) => {
         </button>
         {props.name}
       </span>
-      <Counter />
+      <Counter
+        score={props.score}
+        changeScore={props.changeScore}
+        index={props.index}
+      />
     </div>
   );
 };
