c21ca734b0f7e2d8da23a3beb812a0aaf7a44d17
Arkar Min Tun <arkarmintun1@gmail.com>
2020-12-16 19:31:40 +0630

    react setup with webpack and babel

---
+++ react-setup-webpack-babel/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/style.scss';
+
+const App = () => {
+  return <h1>Hello From React!</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ react-setup-webpack-babel/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+  },
+};
